e9aacfbf340540430403bfa1ff27ef4c4b730d2f
khinatase <gettingsignals@gmail.com>
2022-01-03 11:35:33 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  root 'categories#index'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
-  root 'users#index'
   resources :categories
   get 'categories/new'
   resources :messages
   get 'messages/index'
+  get '/signup', to: 'users#new'
+  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
