d6216877f4ab406e13f0b2c0b24f1ad84c19804e
khinatase <gettingsignals@gmail.com>
2022-01-05 13:57:21 +0900

    #7 ユーザー表示画面でNoMethodError

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @messages = Message.where(user: @user)
+    @messages = Message.where(user: @user).paginate(page: params[:page])
   end
 
   def new
