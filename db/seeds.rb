4849209509849eb1f95f9450b03de75f6487e706
khinatase <gettingsignals@gmail.com>
2022-01-05 16:11:06 +0900

    テストデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,53 @@
 #   )
 
 # end
+
+# admin1 = User.create!(name: "admin1",
+#   password: "password",
+#   password_confirmation: "password",
+#   admin: true
+#   )
+# user1 = User.create!(name: "user1",
+#   password: "password",
+#   password_confirmation: "password",
+#   admin: false
+#   )
+
+# category1 = Category.create!(name: "category1")
+
+# Message.create!(
+#   user_id: admin1.id,
+#   category_id: category1.id,
+#   name: admin1.name,
+#   email: "admin1@email.abc",
+#   subject: "visible=true",
+#   text: "visible=false texttexttext",
+#   visibility: true
+# )
+# Message.create!(
+#   user_id: admin1.id,
+#   category_id: category1.id,
+#   name: admin1.name,
+#   email: "admin1@email.abc",
+#   subject: "subject2",
+#   text: "visible=false texttexttext",
+#   visibility: false
+# )
+# Message.create!(
+#   user_id: user1.id,
+#   category_id: category1.id,
+#   name: user1.name,
+#   email: "user1@email.abc",
+#   subject: "visible=true",
+#   text: "visible=true texttexttext",
+#   visibility: true
+# )
+# Message.create!(
+#   user_id: user1.id,
+#   category_id: category1.id,
+#   name: user1.name,
+#   email: "user1@email.abc",
+#   subject: "visible=false",
+#   text: "visible=false texttexttext",
+#   visibility: false
+# )
